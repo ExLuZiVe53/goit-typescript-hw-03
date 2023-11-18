79af0b764bfd36f347abd2fbdcff747be42d440d
ExLuZiVe53 <imstudent27@gmail.com>
2023-11-18 14:13:39 +0100

    added open my house

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,52 @@
+// const key = new Key();
+
+// const house = new MyHouse(key);
+// const person = new Person(key);
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
+
+// export {};
+
+class Key {
+  constructor(private signature: number = Math.floor(Math.random() * 125)) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean;
+  tenants: Person[];
+  constructor(public key: Key) {
+    this.door = false;
+  }
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +55,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
